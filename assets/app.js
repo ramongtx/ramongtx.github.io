775724ac623637c8b3539416be8c03a39ca5fb7a
Ramon Maciel <ramongtx@gmail.com>
2019-08-18 10:55:02 -0300

    Add professional experiences

--- assets/app.js
+++ assets/app.js
@@ -1,11 +1,12 @@
 // First solve the problem. Then, write the code."
 window.onload = function () {
-    const tw = new Typewriter('#typewriter');
+    const tw = new Typewriter('#typewriter', { loop: true });
     tw.changeDelay(40).
         changeDeleteSpeed(5).
         typeString('First, solve the problem,').
-        pauseFor(3000).
+        pauseFor(3_000).
         deleteAll().
         typeString('then write the code!').
+        pauseFor(10_000).
         start();
 }
